3a0c815429bd2bb14875b153f49cb52f708e3a8d
AlexChek24 <sanek.chekanov99@gmail.com>
2023-01-21 12:54:21 +0300

    v 26

--- engine/mono/GamePlay/SceneData.cs
+++ engine/mono/GamePlay/SceneData.cs
@@ -5,10 +5,11 @@ using Engine;
 public class SceneData : CSComponent {
 
     public static Actor playerCharacter => m_instance != null ? m_instance.m_playerCharacter : null;
+    public static Player player => m_instance != null ? m_instance.m_player : null;
     private static SceneData m_instance = null;
 
     [Open] private Actor m_playerCharacter;
-
+    [Open] private Player m_player;
 
     public override void OnInit() => m_instance = this;
 
